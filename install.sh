acb9f760e6916c56c7270d876f27d35adce886a5
Yohn Aldana <user@localhost>
2025-09-01 19:16:55 -0500

    add base files to be tracked

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create directories
+mkdir -p ~/.config/alacritty
+
+# Create symlinks
+ln -sf ~/.dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/nvim/init.lua ~/.config/nvim/init.lua
