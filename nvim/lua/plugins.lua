8d92d85e830a82eb106e13f43adcff3ba98203b2
Yohn Aldana <user@localhost>
2025-11-10 16:31:35 -0600

    add mason and blink

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,7 +44,7 @@ require("lazy").setup({
       -- C-k: Toggle signature help (if signature.enabled = true)
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
+      keymap = { preset = 'enter' },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -53,7 +53,9 @@ require("lazy").setup({
       },
 
       -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
+      completion = {
+        trigger = { show_on_trigger_character = true },
+        documentation = { auto_show = true } },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
@@ -69,5 +71,7 @@ require("lazy").setup({
       fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }
-  }
+  },
+  -- LSP manager
+  { "mason-org/mason.nvim", opts = {} },
 })
