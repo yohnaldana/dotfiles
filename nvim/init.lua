ec327b40ec96d0b66043abaf576590fcd53ccd13
Yohn Aldana <user@localhost>
2025-11-10 14:34:55 -0600

    create keymaps.lua and require it in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- Yohn's Neovim Configuration
 require('options')
-
+require('keymaps')
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
