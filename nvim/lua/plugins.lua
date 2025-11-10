0c750d1bb98f4296fd1273486301065533dc62ac
Yohn Aldana <user@localhost>
2025-11-10 17:49:43 -0600

    configure nvim-lspconfig for HTML, CSS, JSON, ESLint

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -13,65 +13,125 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-    'soroushsrd/glacier.vim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
-
-    -- use a release tag to download pre-built binaries
-    version = '1.*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'enter' },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
-
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = {
-        trigger = { show_on_trigger_character = true },
-        documentation = { auto_show = true } },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      'soroushsrd/glacier.vim',
+      lazy = false,
+      priority = 1000,
     },
-    opts_extend = { "sources.default" }
-  },
-  -- LSP manager
-  { "mason-org/mason.nvim", opts = {} },
+    {
+      'saghen/blink.cmp',
+      -- optional: provides snippets for the snippet source
+      dependencies = { 'rafamadriz/friendly-snippets' },
+
+      -- use a release tag to download pre-built binaries
+      version = '1.*',
+      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+      -- build = 'cargo build --release',
+      -- If you use nix, you can build from source using latest nightly rust with:
+      -- build = 'nix run .#build-plugin',
+
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = 'enter' },
+
+        appearance = {
+          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- Adjusts spacing to ensure icons are aligned
+          nerd_font_variant = 'mono'
+        },
+
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = {
+          trigger = { show_on_trigger_character = true },
+          documentation = { auto_show = true } },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+      },
+      opts_extend = { "sources.default" }
+    },
+    -- LSP manager
+    { "mason-org/mason.nvim", opts = {} },
+    {
+  "neovim/nvim-lspconfig",
+  config = function()
+    -- When the LSP attaches to a buffer
+    local on_attach = function(_, bufnr)
+      local opts = { buffer = bufnr, silent = true }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    end
+
+    --------------------------------------------------------------------
+    -- HTML
+    --------------------------------------------------------------------
+    vim.lsp.config["html"] = {
+      cmd = { "vscode-html-language-server", "--stdio" },
+      filetypes = { "html" },
+      root_dir = vim.fs.root(0, { ".git", "index.html" }),
+      on_attach = on_attach,
+    }
+    vim.lsp.enable("html")
+
+    --------------------------------------------------------------------
+    -- CSS
+    --------------------------------------------------------------------
+    vim.lsp.config["cssls"] = {
+      cmd = { "vscode-css-language-server", "--stdio" },
+      filetypes = { "css", "scss", "less" },
+      root_dir = vim.fs.root(0, { ".git", "package.json" }),
+      on_attach = on_attach,
+    }
+    vim.lsp.enable("cssls")
+
+    --------------------------------------------------------------------
+    -- JSON
+    --------------------------------------------------------------------
+    vim.lsp.config["jsonls"] = {
+      cmd = { "vscode-json-language-server", "--stdio" },
+      filetypes = { "json", "jsonc" },
+      root_dir = vim.fs.root(0, { ".git", "package.json" }),
+      on_attach = on_attach,
+    }
+    vim.lsp.enable("jsonls")
+
+    --------------------------------------------------------------------
+    -- ESLint
+    --------------------------------------------------------------------
+    vim.lsp.config["eslint"] = {
+      cmd = { "vscode-eslint-language-server", "--stdio" },
+      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      root_dir = vim.fs.root(0, { ".eslintrc.*", "package.json", ".git" }),
+      on_attach = on_attach,
+    }
+    vim.lsp.enable("eslint")
+  end,
+}
+
+
 })
