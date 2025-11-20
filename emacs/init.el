c9ebe05a6881d2874daa8c5cd8c63f22f8ab70ca
Yohn Aldana <user@localhost>
2025-11-19 21:07:07 -0600

    update require symbol to 'org-config

--- emacs/init.el
+++ emacs/init.el
@@ -26,6 +26,20 @@
 (require 'ui)
 (require 'editing)
 (require 'keybinds)
-(require 'packages)
+(require 'org-config)
+;; (require 'packages)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("17570f818a8a3877994453342e3425a3b4fa4b3ebac050b4ecbbee958f1ca133" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
