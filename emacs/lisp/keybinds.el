34b4408418e00f8c7f082263758b929bb98c9945
Yohn Aldana <user@localhost>
2025-11-19 21:08:36 -0600

    initial commit of keybinds.el; paragraph motions update

---
+++ emacs/lisp/keybinds.el
@@ -0,0 +1,8 @@
+;;; keybinds.el --- Personal Keybindings -*- lexical-binding: t; -*-
+
+;; Paragraph motions
+(global-set-key (kbd "M-n") #'forward-paragraph)
+(global-set-key (kbd "M-p") #'backward-paragraph)
+
+(provide 'keybinds)
+;;; keybinds.el ends here
