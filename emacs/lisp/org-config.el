9601bea81f85f6ac24d5bfc62768169878b97ffd
Yohnathan Aldana <user@localhost>
2025-11-22 00:11:49 -0600

    add org-config.el to emacs files tracked

---
+++ emacs/lisp/org-config.el
@@ -0,0 +1,6 @@
+;;; ;;; org.el --- Org configuratino file -*- lexical-binding: t; -*- 
+
+(add-hook 'org-mode-hook (lambda () (display-line-numbers-mode 0)))
+
+(provide 'org-config)
+;; org.el ends here
